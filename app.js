6646d3d7ab840dce5356eff07bccfa6406b5761e
ABEER QURESHI <abiyaqureshi16@gmail.com>
2025-12-06 10:46:05 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const {log} = console;
+const express = require('express')
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+
+
+io.on('connection',(socket)=>{
+    console.log('a user connected',socket.id);
+    socket.on('message',(msg)=>{
+        io.emit('message',msg)
+    })
+});
+
+
+server.listen(3000, () => {
+    console.log('Server is running on port');
+});
