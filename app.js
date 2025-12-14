eaa616831ae732d18935264db4fd353bde2177f5
abiyaq <abiyaqureshi5@gmail.com>
2025-12-14 12:13:29 +0500

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const {log} = console;
 const express = require('express')
 const app = express();
-const Port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const http = require('http');
 const server = http.createServer(app);
 const {Server} = require('socket.io');
